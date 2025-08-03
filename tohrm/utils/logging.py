9e9833b61e0f23d7243611b7bef6b68566fb1465
symbiosisnm <nmartificialintelligence@gmail.com>
2025-08-03 07:05:50 -0700

    Create logging.py

---
+++ tohrm/utils/logging.py
@@ -0,0 +1,11 @@
+# Set up a consistent logger.
+import logging
+
+
+def get_logger(name: str, level: int = logging.INFO):
+    """
+    Configure and return a logger that writes to stdout with timestamps.
+    Avoids adding duplicate handlers if called multiple times.
+    """
+    # TODO: create logger, set level, and attach handler with formatter
+    pass
